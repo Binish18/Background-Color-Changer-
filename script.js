ba4338f36a8d4c7852e791b7517ca24faab9951b
Binish <beenishwaqar1234@gmail.com>
2022-09-11 08:57:25 +0500

    Message

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnEL = document.querySelector(".button");
+const bodyEl = document.querySelector("body")
+btnEL.addEventListener("click",()=>{
+       bodyEl.style.backgroundColor = "red";
+})
